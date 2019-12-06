e6d8a21475d7643456c130ff6440f78b2bc5efe3
Joey Marianer <rmarianer@instructure.com>
2019-12-06 11:25:14 +0100

    Puzzle 3 part II

--- 2019/3.py
+++ 2019/3.py
@@ -1,6 +1,7 @@
 def locations(wire):
     cur = 0,0
-    ret = [cur]
+    dist = 0
+    ret = {cur: dist}
     for inst in wire.split(','):
         dir = inst[0]
         count = int(inst[1:])
@@ -15,15 +16,23 @@ def locations(wire):
             delta = 0,-1
 
         for i in range(count):
-            cur = (cur[0] + delta[0], cur[1] + delta[1])
-            ret.append(cur)
+            cur = cur[0] + delta[0], cur[1] + delta[1]
+            dist += 1
+            if cur not in ret:
+                ret[cur] = dist
 
     return ret
 
 
-wire1=input()
-wire2=input()
+wire1=locations(input())
+wire2=locations(input())
 
-intersections = set(locations(wire1)).intersection(set(locations(wire2)))
+intersections = set(wire1).intersection(wire2)
+
+# Part I
 distances = [abs(i[0]) + abs(i[1]) for i in intersections]
 print(sorted(distances)[1])
+
+# Part II
+timings = [wire1[i] + wire2[i] for i in intersections]
+print(sorted(timings)[1])
