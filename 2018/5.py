d93bf67fc6af375d7ea281aaa463887ee2200ae3
Joey Marianer <rmarianer@instructure.com>
2019-12-10 10:25:36 +0100

    Puzzle 5 part II

--- 2018/5.py
+++ 2018/5.py
@@ -1,13 +1,22 @@
 polymer=input()
 letters='abcdefghijklmnopqrstuvwxyz'
 
-while True:
-    old_polymer = polymer
-    for letter in letters:
-        polymer = polymer.replace(letter + letter.upper(), '')
-        polymer = polymer.replace(letter.upper() + letter, '')
+def react(polymer):
+    while True:
+        old_polymer = polymer
+        for letter in letters:
+            polymer = polymer.replace(letter + letter.upper(), '')
+            polymer = polymer.replace(letter.upper() + letter, '')
 
-    if old_polymer == polymer:
-        break
+        if old_polymer == polymer:
+            return polymer
 
-print(len(polymer))
+# Part I
+print(len(react(polymer)))
+
+# Part II
+lens = {}
+for letter in letters:
+    polymer1 = polymer.replace(letter, '').replace(letter.upper(), '')
+    lens[letter] = len(react(polymer1))
+print(min(lens.values()))
