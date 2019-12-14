6d829d1bbccd9b4e63f25a37dcbd60b7c2c5662e
Joey Marianer <rmarianer@instructure.com>
2019-12-14 11:01:06 +0000

    Puzzle 12, Part I

---
+++ 2019/12.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+positions = np.array([
+    [7, 10, 17],
+    [-2, 7, 0],
+    [12, 5, 12],
+    [5, -8, 6]])
+
+velocities = np.zeros((4,3), dtype=np.int16)
+
+for _ in range(1000):
+    for i in range(4):
+        for j in range(4):
+            for x in range(3):
+                if positions[i][x] > positions[j][x]:
+                    velocities[i][x] -= 1
+                if positions[i][x] < positions[j][x]:
+                    velocities[i][x] += 1
+    positions += velocities
+
+tot = 0
+for i in range(4):
+    tot += sum(abs(positions[i])) * sum(abs(velocities[i]))
+print(tot)
