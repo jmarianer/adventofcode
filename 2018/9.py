f33c80ee8d8d71c769884c247219d77e77ea505f
Joey Marianer <rmarianer@instructure.com>
2019-12-19 19:17:31 -0800

    Puzzle 9 part I

---
+++ 2018/9.py
@@ -0,0 +1,15 @@
+ELVES = 471
+MAX = 72026
+
+# Current marble is first; clockwise is to the right
+circle = [3, 0, 2, 1]
+
+scores = [0]  * ELVES
+for marble in range(4, MAX + 1):
+    if marble % 23 == 0:
+        scores[marble % ELVES] += marble + circle[-7]
+        circle = circle[-6:] + circle[:-7]
+    else:
+        circle = [marble] + circle[2:] + circle[0:2]
+
+print(max(scores))
