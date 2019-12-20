0c93b037b4a9b63b45e00429f5cfea8136bee71e
Joey Marianer <rmarianer@instructure.com>
2019-12-19 18:35:38 -0800

    Change to the way directions are calculated

--- 2019/10.py
+++ 2019/10.py
@@ -3,17 +3,23 @@ from fractions import Fraction
 
 asteroid_locations = []
 for i, line in enumerate(sys.stdin.readlines()):
-    asteroid_locations.extend((i, j) for j in range(len(line)) if line[j] == '#')
+    asteroid_locations.extend((j, i) for j in range(len(line)) if line[j] == '#')
 
-def slope(loc1, loc2):
+def direction(loc1, loc2):
     if loc1[1] == loc2[1]:
-        return 'up' if loc2[0] - loc1[0] > 0 else 'down'
+        return (-1, 0) if loc2[0] > loc1[0] else (1, 0)
     
-    dir = 'left' if loc2[1] - loc1[1] > 0 else 'right'
-    return (dir, Fraction(loc1[0] - loc2[0], loc1[1] - loc2[1]))
+    frac = Fraction(loc1[0] - loc2[0], loc1[1] - loc2[1])
+    dir = -1 if loc2[1] > loc1[1] else 1
+    return (dir * frac.numerator, dir * frac.denominator)
 
-detected = []
+detector = ()
+detected = set()
 for loc in asteroid_locations:
-    detected.append(len(set(slope(loc2, loc) for loc2 in asteroid_locations if loc2 != loc)))
+    current_detected = set(direction(loc2, loc) for loc2 in asteroid_locations if loc2 != loc)
+    if len(detected) < len(current_detected):
+        detector = loc
+        detected = current_detected
 
-print(max(detected))
+print(detector)
+print(len(detected))
