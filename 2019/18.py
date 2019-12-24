337488be72723c217b02ab9f140a689a20e3fbec
Joey Marianer <rmarianer@instructure.com>
2019-12-24 12:42:31 -0800

    Finally solved puzzle 18, after a very obvious-in-hindsight hint from Reddit.

--- 2019/18.py
+++ 2019/18.py
@@ -1,5 +1,6 @@
 import sys
-import queue as q
+from queue import Queue, PriorityQueue
+from utils import queue_iterator
 
 lines = sys.stdin.readlines()
 
@@ -15,13 +16,9 @@ for i in range(i_max):
         if lines[i][j] in KEYS or lines[i][j] == '@':
             locations[lines[i][j]] = (i, j)
 
-def queue_iterator(q):
-    while not q.empty():
-        yield q.get()
-
 getting_to = {}
 for origin, (i, j) in locations.items():
-    queue = q.Queue()
+    queue = Queue()
     queue.put((i, j, 0, []))
     visited = set()
     for (i, j, distance, doors) in queue_iterator(queue):
@@ -43,18 +40,25 @@ for origin, (i, j) in locations.items():
 
 all_keys = set(locations.keys()) - {'@'}
 
-lowest_distance = 999999
-def traverse_graph(current_key, length, path):
-    global lowest_distance
-    if length >= lowest_distance:
-        return
-    have_keys = set(path)
-    if have_keys == all_keys:
-        lowest_distance = length
-        print(length, path)
-    for next_key in all_keys - have_keys:
-        distance, need_keys = getting_to[current_key, next_key]
-        if need_keys.issubset(have_keys):
-            traverse_graph(next_key, length + distance, path + [next_key])
+# Gah! I tried to DFS it when I should have used Dijkstra. Somehow I didn't
+# realize what I should have done until I saw a reference online, and then it
+# suddenly became obvious.
+visited = set()
+queue = PriorityQueue()
+queue.put((0, '@', ''))
 
-traverse_graph('@', 0, [])
+for (distance, location, path) in queue_iterator(queue):
+    have_keys = set(path)
+    keys_string = ''.join(sorted(have_keys))
+    if (location, keys_string) in visited:
+        continue
+    visited.add((location, keys_string))
+
+    if have_keys == all_keys:
+        print(distance, path)
+        break
+
+    for next_key in all_keys - have_keys:
+        added_distance, need_keys = getting_to[location, next_key]
+        if need_keys.issubset(have_keys):
+            queue.put((distance + added_distance, next_key, path + next_key))
