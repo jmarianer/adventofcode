cbfa61bbb09dcd2641ac4bf7db492dd7e3aaff22
Joey Marianer <rmarianer@instructure.com>
2020-07-26 21:43:01 -0700

    Day 18 part II

--- 2018/18.py
+++ 2018/18.py
@@ -1,9 +1,27 @@
 import sys
 from copy import deepcopy
 
+
+def state2str(state):
+    return ''.join(''.join(line) for line in state)
+
+
 def printstate(state):
     for line in state:
         print(''.join(line))
+
+
+def printvalue(state):
+    lumber = 0
+    trees = 0
+    for line in state:
+        for char in line:
+            if char == '|':
+                trees += 1
+            if char == '#':
+                lumber += 1
+
+    print(lumber*trees)
 
 
 def count(state, i, j, c):
@@ -18,12 +36,7 @@ def count(state, i, j, c):
     return count
 
 
-state = [['║'] + list(line.strip()) + ['║'] for line in sys.stdin.readlines()]
-w = len(state[0])
-h = len(state)
-state = [['╔'] + ['═'] * (w - 2) + ['╗']] + state + [['╚'] + ['═'] * (w - 2) + ['╝']]
-
-for _ in range(10):
+def oneminute(state):
     newstate = deepcopy(state)
     for i in range(1, h + 1):
         for j in range(1, w - 1):
@@ -33,17 +46,33 @@ for _ in range(10):
                 newstate[i][j] = '#'
             if state[i][j] == '#' and (count(state, i, j, '|') < 1 or count(state, i, j, '#') < 1):
                 newstate[i][j] = '.'
+    return newstate
 
-    state = newstate
 
+# Box drawing is so that we don't need to worry about overflow. Also it makes
+# printing prettier, and me nostalgic for the DOS days.
+state = [['║'] + list(line.strip()) + ['║'] for line in sys.stdin.readlines()]
+w = len(state[0])
+h = len(state)
+state = [['╔'] + ['═'] * (w - 2) + ['╗']] + state + [['╚'] + ['═'] * (w - 2) + ['╝']]
 
-lumber = 0
-trees = 0
-for line in state:
-    for char in line:
-        if char == '|':
-            trees += 1
-        if char == '#':
-            lumber += 1
+# Part I
+for it in range(10):
+    state = oneminute(state)
+printvalue(state)
 
-print(lumber*trees)
+# Part II
+seenstates = {}
+for it in range(10, 10000):
+    state = oneminute(state)
+    mystr = state2str(state)
+    if mystr in seenstates:
+        break
+
+    seenstates[mystr] = it
+
+looplength = it - seenstates[mystr]
+it += int((1000000000 - it) / looplength) * looplength
+for it in range(it + 1, 1000000000):
+    state = oneminute(state)
+printvalue(state)
