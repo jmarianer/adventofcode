1c1d051956024bee4c2d8dc152fcd6d27775f92b
Joey Marianer <rmarianer@instructure.com>
2020-07-27 09:57:39 -0700

    Day 22 part I

--- 2018/20.py
+++ 2018/20.py
@@ -11,6 +11,7 @@ def longest(alts):
     return val
 
 s = input().strip()[1:-1]
+orig_s = s
 
 for _ in range(1000000):
     m = re.search("\([NEWS|]*\)", s)
@@ -29,3 +30,34 @@ for _ in range(1000000):
     s = s[:start] + s[end:]
 
 print(len(s))
+
+
+grid = []
+for _ in range(50):
+    grid.append(['#'] * 50)
+print(s)
+
+def populategrid(i, j, s):
+    print(i,j)
+    if grid[i][j] != '#':
+        return
+    grid[i][j] = '.'
+    if s == '':
+        return
+    if s[0] == 'N':
+        grid[i-1][j] = '-'
+        populategrid(i-2, j, s[1:])
+    if s[0] == 'S':
+        grid[i+1][j] = '-'
+        populategrid(i+2, j, s[1:])
+    if s[0] == 'W':
+        grid[i][j-1] = '|'
+        populategrid(i, j-2, s[1:])
+    if s[0] == 'E':
+        grid[i][j+1] = '|'
+        populategrid(i, j+2, s[1:])
+
+populategrid(25, 25, s)
+grid[25][25] = 'S'
+for line in grid:
+    print(''.join(line))
