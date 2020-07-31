549481a8d10800b637992537881cf08a8e6f1217
Joey Marianer <rmarianer@instructure.com>
2020-07-30 23:52:11 -0700

    Day 16 part II

--- 2017/16.hs
+++ 2017/16.hs
@@ -1,3 +1,4 @@
+import Data.Char
 import Data.List
 import Data.List.Index
 import Data.List.Split
@@ -22,7 +23,30 @@ move order ('p':a:'/':b:[]) = exchange a' b' order
   where a' = fromJust $ elemIndex a order
         b' = fromJust $ elemIndex b order
 
+runPermutation source permutation = map (\c -> source !! (ord c - ord 'a')) permutation
+
+permutationPower :: Int -> String -> String
+permutationPower n permutation =
+  if (n == 1)
+  then permutation
+  else if even n
+       then let halfPerm = permutationPower (n `div` 2) permutation
+             in runPermutation halfPerm halfPerm
+       else runPermutation (permutationPower (n-1) permutation) permutation
+
+
+lastChar = 'p'
 main = do
   c <- getContents
-  let moves = splitOn "," c
-  print $ foldl move ['a'..'p'] moves
+  let moves = splitOn "," $ dropWhileEnd isSpace c
+  print $ foldl move ['a'..lastChar] moves
+
+  let numberMoves = filter (\m -> head m /= 'p') moves
+  let numberPermutation = foldl move ['a'..lastChar] numberMoves
+  let letterMoves = filter (\m -> head m == 'p') moves
+  let letterPermutation = foldl move ['a'..lastChar] letterMoves
+
+  let times = 1000000
+  let afterNumbers = runPermutation ['a'..lastChar] (permutationPower times numberPermutation)
+  let afterLetters = runPermutation (permutationPower times letterPermutation) afterNumbers    
+  print afterLetters
