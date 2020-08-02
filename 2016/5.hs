ac276e0c9505f592e6ef3d5a481f6fffe1f29f88
Joey Marianer <rmarianer@instructure.com>
2020-08-02 12:55:11 -0700

    Day 5 complete

--- 2016/5.hs
+++ 2016/5.hs
@@ -9,6 +9,10 @@ import qualified Crypto.Hash.MD5 as MD5
 
 import Data.Text.Encoding
 import Data.Char
+import Data.List
+import Data.Maybe
+
+digit = (+48)
 
 hashes :: T.Text -> [BS.ByteString]
 hashes s = map hash [0..]
@@ -16,12 +20,14 @@ hashes s = map hash [0..]
         hash n = Hex.encode $ MD5.hash $ encodeUtf8 $ T.concat [s, T.pack $ show n]
 
 interestingHash :: BS.ByteString -> Bool
-interestingHash d = BS.take 5 d == "00000" && BS.index d 5 `elem` [48..55]
-
-
+interestingHash d = BS.take 5 d == "00000" && BS.index d 5 `elem` [digit 0..digit 7]
 
 main = do
-  --print $ BS.pack $ map (\d -> BS.index d 5) $ take 8 $ filter (\d -> BS.take 5 d == "00000") $ hashes "reyedfim"
+  let myHashes = hashes "reyedfim"
+  print $ BS.pack $ map (\d -> BS.index d 5) $ take 8 $ filter (\d -> BS.take 5 d == "00000") myHashes
 
-  let locletter = map (\d -> (BS.index d 5 - 48, BS.index d 6)) $ filter interestingHash $ hashes "abc"
 
+  let locletter = map (\d -> (BS.index d 5 - digit 0, BS.index d 6)) $ filter interestingHash myHashes
+      locletters = map (\n -> find ((==n) . fst) locletter) [0..7]
+      letters = map (snd . fromJust) locletters
+  print $ BS.pack letters
