364e8af4c80aaee0c983037c48dac04d74bf4436
Joey Marianer <rmarianer@instructure.com>
2020-08-02 18:11:06 -0700

    Simple BFS + day 13 part I

---
+++ 2016/13.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Site
+
+import qualified Data.Map as Map
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Data.Bits
+import Data.List
+
+{- Test data:
+input = 10
+target = (7, 4)
+-}
+input = 1350
+target = (31, 39)
+
+open :: (Int, Int) -> Bool
+open (x, y) = even $ popCount (x*x + 3*x + 2*x*y + y + y*y + input)
+
+draw True = '.'
+draw False = '#'
+
+adjacent :: (Int, Int) -> [((Int, Int), Direction)]
+adjacent (x, y) = [((x, y+1), Dn),
+                   ((x, y-1), Up),
+                   ((x-1, y), Lt),
+                   ((x+1, y), Rt)]
+adjacentAndOpen = filter valid . adjacent
+  where valid ((x, y), _) = x>=0 && y>=0 && open (x, y)
+
+main = do
+  -- debugging:
+  -- TIO.putStrLn $ T.intercalate "\n" $ map (\y -> (T.pack $ map (\x -> draw $ open (x, y)) [0..9])) [0..9]
+  let directions = bfs (1, 1) adjacentAndOpen (== target)
+      Just dir = Map.lookup target directions
+  print $ length $ dir
--- 2016/Site.hs
+++ 2016/Site.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
+
 module Site where
+
+import qualified Data.Map as Map
+import qualified Data.Sequence as Seq
 
 data Direction = Up | Dn | Lt | Rt deriving (Show, Eq)
 
@@ -26,3 +32,29 @@ move Dn (i, j) = (i+1, j)
 move Lt (i, j) = (i, j-1)
 move Rt (i, j) = (i, j+1)
 
+-- Insert a value into a map, but only if it wasn't already there.
+maybeInsert :: Ord k => k -> v -> Map.Map k v -> Map.Map k v
+maybeInsert k v map = Map.alter alteration k map
+  where alteration (Just o) = Just o
+        alteration Nothing  = Just v
+
+bfs :: forall a b . (Ord a, Eq a) =>
+       a -- start
+    -> (a -> [(a, b)]) -- adjacency
+    -> (a -> Bool) -- goal
+    -> Map.Map a [b] -- directions (to everywhere else in the graph)
+bfs start adjacency goal = bfs' (Map.singleton start []) (Seq.singleton start)
+  where bfs' :: Map.Map a [b] -> Seq.Seq a -> Map.Map a [b]
+        bfs' directions Seq.Empty = directions
+        bfs' directions (current Seq.:<| rest) =
+          if goal current
+          then directions
+          else bfs' (directions `Map.union` newDirections) newQueue
+            where adjacent :: [(a, b)]
+                  adjacent = filter (\(a, _) -> not $ a `Map.member` directions) $ adjacency current
+                  currentDirections :: [b]
+                  Just currentDirections = Map.lookup current directions
+                  newDirections ::  Map.Map a [b]
+                  newDirections = Map.fromList $ map (\(a, b) -> (a, b:currentDirections)) adjacent
+                  newQueue :: Seq.Seq a
+                  newQueue = rest Seq.>< Seq.fromList (map fst adjacent)
