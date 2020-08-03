8a94b28194611f9fbe32ac1e1dfbd6dfcef12554
Joey Marianer <rmarianer@instructure.com>
2020-08-02 21:57:02 -0700

    Factor hashes out of day 5

--- 2016/5.hs
+++ 2016/5.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Site
 
+import qualified Crypto.Hash.MD5 as MD5
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Base16 as Hex
-import qualified Crypto.Hash.MD5 as MD5
 
 import Data.Text.Encoding
 import Data.Char
@@ -14,16 +14,11 @@ import Data.Maybe
 
 digit = (+48)
 
-hashes :: T.Text -> [BS.ByteString]
-hashes s = map hash [0..]
-  where hash :: Int -> BS.ByteString
-        hash n = Hex.encode $ MD5.hash $ encodeUtf8 $ T.concat [s, T.pack $ show n]
-
 interestingHash :: BS.ByteString -> Bool
 interestingHash d = BS.take 5 d == "00000" && BS.index d 5 `elem` [digit 0..digit 7]
 
 main = do
-  let myHashes = hashes "reyedfim"
+  let myHashes = hashes (Hex.encode . MD5.hash) "reyedfim"
   print $ BS.pack $ map (\d -> BS.index d 5) $ take 8 $ filter (\d -> BS.take 5 d == "00000") myHashes
 
 
