8fd7b04e106cc56d6c02e6585b4bb480ea9f3068
Joey Marianer <rmarianer@instructure.com>
2020-08-03 16:46:10 -0700

    Day 22 part II manual solution

--- 2016/22.hs
+++ 2016/22.hs
@@ -37,3 +37,16 @@ main = do
   let nodes = rights $ map (parseOnly parseLine) $ T.lines c
   let viablePairs = [(n1, n2) | n1 <- nodes, n2 <- nodes, n1 /= n2, avail n2 > used n1, used n1 /= 0]
   print $ length viablePairs
+
+{- This only solves part I. I did part II manually by noticing the similarity
+ - with the sample, and from vast experience doing 15-puzzles. :-)
+ -
+ - The input I got has a row of blockers (huge, full cells whose data is going
+ - nowhere) at y=12, x=6..37, and an empty cell at (16, 23). We move this empty
+ - cell to (6, 23) (11 moves), then to (5, 0) (23 moves), and then to (37, 0)
+ - (32 moves) where the goal was originally. Now the goal is at (36, 0) and the
+ - empty cell is to its right. To move the goal one space to the left takes
+ - five moves (down-left-left-up-right), and we need to repeat that 36 times.
+ -
+ - Therefore the total number of moves is 11 + 23 + 32 + 36*5 = 246.
+ -}
