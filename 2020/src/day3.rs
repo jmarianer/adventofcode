d8d48a51e24a0b8aaab4a51fc3faf2af8dd70a95
Joey Marianer <rmarianer@instructure.com>
2020-12-02 21:25:13 -0800

    2020 day 3. I don't know what's up with the references there. Maybe I'll come back to it later.

---
+++ 2020/src/day3.rs
@@ -0,0 +1,29 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+//fn check_slope_1<I>(lines: I, right: usize) -> usize where I : Iterator<Item = io::Result<String>> {
+fn check_slope_1(lines: &Vec<&io::Result<String>>, right: usize) -> usize {
+    lines.iter().enumerate().filter(|(i, line)| {
+        let line = line.as_ref().unwrap();
+        let ch   = line.chars().nth((i * right) % line.len()).unwrap();
+        ch == '#'
+    }).count()
+}
+
+pub fn day3() -> io::Result<()> {
+    let file = File::open("input3")?;
+    let reader = BufReader::new(file);
+    let lines_orig = reader.lines().collect::<Vec<_>>();
+
+    let lines = lines_orig.iter().step_by(1).collect::<Vec<_>>();
+    println!("{}", check_slope_1(&lines, 3));
+
+    let every_other_line = lines_orig.iter().step_by(2).collect::<Vec<_>>();
+    println!("{}",
+             check_slope_1(&lines, 1) *
+             check_slope_1(&lines, 3) *
+             check_slope_1(&lines, 5) *
+             check_slope_1(&lines, 7) *
+             check_slope_1(&every_other_line, 1));
+    Ok(())
+}
--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -1,9 +1,8 @@
 use std::io::{self};
 mod day1;
-mod day2;
+mod day3;
 
 fn main() -> io::Result<()> {
-    day1::day1();
-    day2::day2()?;
+    day3::day3()?;
     Ok(())
 }
