c5b97f9bda15ef2facdec2ff7d50ff7044ca497b
Joey Marianer <me@joeym.org>
2021-12-23 13:40:12 -0800

    Now runs in about 20 seconds for part I

--- 2021/23.js
+++ 2021/23.js
@@ -1,43 +1,75 @@
 'use strict'
-let PriorityQueue = require('priorityqueuejs');
+const PriorityQueue = require('priorityqueuejs');
 
 // Test input
-//let original_state = '.......BCBDADCA'
+//const original_state = '.......BCBDADCA'
 
 // Real input
-let original_state = '.......DACDCABB'
+const original_state = '.......DACDCABB'
 
-let target_state = '.......ABCDABCD'
-let queue = new PriorityQueue((x, y) => y[1] - x[1])
-queue.enq([original_state, 0])
 
-let transitions = [
-  // from, [to], energy multiplier
-  [1, [2], 1],
-  [2, [1], 1],
-  [2, [3, 8], 2],
-  [3, [2, 8, 9, 4], 2],
-  [4, [3, 9, 10, 5], 2],
-  [5, [4, 10, 11, 6], 2],
-  [6, [5, 11], 2],
-  [6, [7], 1],
-  [7, [6], 1],
-  [8, [2, 3], 2],
-  [9, [3, 4], 2],
-  [10, [4, 5], 2],
-  [11, [5, 6], 2],
-  [8, [12], 1],
-  [9, [13], 1],
-  [10, [14], 1],
-  [11, [15], 1],
-  [12, [8], 1],
-  [13, [9], 1],
-  [14, [10], 1],
-  [15, [11], 1],
+const target_state = '.......ABCDABCD'
+
+// "lower" means the additional amount of energy to lower an amphipod into a
+// chamber that isn't its own. We do this if we must, but we probably won't
+// have to.
+const base_energy = { A: 1, B: 10, C: 100, D: 1000, lower: 1000000 }
+
+
+const top_two_row_transitions = [
+  // Top row
+  [0, 1, 1],
+  [1, 0, 1],
+  [1, 2, 2],
+  [2, 1, 2],
+  [2, 3, 2],
+  [3, 2, 2],
+  [3, 4, 2],
+  [4, 3, 2],
+  [4, 5, 2],
+  [5, 4, 2],
+  [5, 6, 1],
+  [6, 5, 1],
+
+  // Top to second and back
+  [1, 7, 2],
+  [2, 7, 2],
+  [2, 8, 2],
+  [3, 8, 2],
+  [3, 9, 2],
+  [4, 9, 2],
+  [4, 10, 2],
+  [5, 10, 2],
+  [7, 1, 2],
+  [7, 2, 2],
+  [8, 2, 2],
+  [8, 3, 2],
+  [9, 3, 2],
+  [9, 4, 2],
+  [10, 4, 2],
+  [10, 5, 2],
 ]
 
-let base_energy = { A: 1, B: 10, C: 100, D: 1000 }
+function add_new_state(state, from, to, energy_to_here, energy) {
+  let new_state = Array.from(state)
+  new_state[to] = state[from]
+  new_state[from] = '.'
+
+  // Add extra energy when lowering an amphipod into the wrong chamber.
+  if (to > 7 && to > from && state[from] !== target_state[to]) {
+    energy_to_here += base_energy.lower
+  }
+  queue.enq([
+    new_state.join(''),
+    energy_to_here + energy * base_energy[state[from]]
+  ])
+}
+
+
+// Entries in the queue: state, 
+let queue = new PriorityQueue((x, y) => y[1] - x[1])
 let visited = new Set()
+queue.enq([original_state, 0])
 
 while (queue.size() > 0) {
   let [state, energy_to_here] = queue.deq()
@@ -45,22 +77,34 @@ while (queue.size() > 0) {
     continue
   }
   visited.add(state)
+
   if (state === target_state) {
     console.log(energy_to_here)
     break
   }
 
-  for (let [from, tos, energy] of transitions) {
-    for (let to of tos) {
-      let from_elt = state[from - 1]
-      if (from_elt !== '.' && state[to - 1] === '.') {
-        let new_state = Array.from(state)
-        new_state[to - 1] = from_elt
-        new_state[from - 1] = '.'
-        if (!visited.has(new_state)) {
-          queue.enq([new_state.join(''), energy_to_here + energy * base_energy[from_elt]])
-        }
-      }
+  if (energy_to_here > 100000) {
+    console.log('wat')
+    break
+  }
+
+  for (let [from, to, energy] of top_two_row_transitions) {
+    if (state[from] === '.' || state[to] !== '.') {
+      continue
+    }
+
+    add_new_state(state, from, to, energy_to_here, energy)
+  }
+
+  for (let chamber = 0; chamber < 4; chamber++) {
+    let row2 = chamber + 7
+    let row3 = chamber + 11
+
+    if (state[row2] === '.' && state[row3] !== '.') {
+      add_new_state(state, row3, row2, energy_to_here, 1)
+    }
+    if (state[row2] !== '.' && state[row3] === '.') {
+      add_new_state(state, row2, row3, energy_to_here, 1)
     }
   }
 }
